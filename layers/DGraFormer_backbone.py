fd531deb13d9a8f16e9fc049a5f33463b5dbbc27
Yan  Han <143077869+yh-Hanniel@users.noreply.github.com>
2025-02-03 02:35:07 +0800

    Add files via upload

--- layers/DGraFormer_backbone.py
+++ layers/DGraFormer_backbone.py
@@ -25,9 +25,8 @@ class DGraFormer_backbone(nn.Module):
                  act: str = "gelu", key_padding_mask: bool = 'auto',
                  padding_var: Optional[int] = None, attn_mask: Optional[Tensor] = None, res_attention: bool = True,
                  pre_norm: bool = False, store_attn: bool = False,
-                 pe: str = 'zeros', learn_pe: bool = True, fc_dropout: float = 0., head_dropout=0, padding_patch=None,
-                 pretrain_head: bool = False, head_type='flatten', individual=False, revin=True, affine=True,
-                 subtract_last=False,
+                 pe: str = 'zeros', learn_pe: bool = True, fc_dropout: float = 0., head_dropout=0, revin=True,
+                 affine=True, subtract_last=False,
                  verbose: bool = False, **kwargs):
 
         super().__init__()
@@ -43,29 +42,28 @@ class DGraFormer_backbone(nn.Module):
         print(f"patch_num: {patch_num}")
         print(f"patch_len: {patch_len}")
         # Backbone
-        self.backbone = TSTiEncoder(c_in, patch_num=patch_num, patch_len=patch_len, max_seq_len=max_seq_len,
-                                    n_layers=n_layers, d_model=d_model, n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff,
-                                    attn_dropout=attn_dropout, dropout=dropout, act=act,
-                                    key_padding_mask=key_padding_mask, padding_var=padding_var,
-                                    attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm,
-                                    store_attn=store_attn,
-                                    pe=pe, learn_pe=learn_pe, verbose=verbose, **kwargs)
+        self.mtt = MTT(c_in, patch_num=patch_num, patch_len=patch_len, max_seq_len=max_seq_len,
+                       n_layers=n_layers, d_model=d_model, n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff,
+                       attn_dropout=attn_dropout, dropout=dropout, act=act,
+                       key_padding_mask=key_padding_mask, padding_var=padding_var,
+                       attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm,
+                       store_attn=store_attn,
+                       pe=pe, learn_pe=learn_pe, verbose=verbose, **kwargs)
 
         # Head
         self.head_nf = d_model * patch_num
         self.n_vars = c_in
         self.d_gcn = d_gcn
-        self.head = Flatten_Head(self.n_vars, self.head_nf, target_window, head_dropout=head_dropout)
+        self.pred = Predictor(self.n_vars, self.head_nf, target_window, head_dropout=head_dropout)
 
         self.device = device
         self.d_graph = d_graph
         self.mask = mask
         self.mp_layers = mp_layers
 
-        self.gc = graph_constructor(self.n_vars, self.d_graph, self.device, mask=self.mask)
-        self.gcn1 = DGCN(d_gcn=self.d_gcn, mp_layers=self.mp_layers)
-        self.gcn2 = DGCN(d_gcn=self.d_gcn, mp_layers=self.mp_layers)
-
+        self.gc = Graph_constructor(self.n_vars, self.d_graph, self.device, mask=self.mask)
+        self.dcgl1 = DCGL(d_gcn=self.d_gcn, mp_layers=self.mp_layers)
+        self.dcgl2 = DCGL(d_gcn=self.d_gcn, mp_layers=self.mp_layers)
 
     def forward(self, z, time_index, current_epoch):  # z: [bs x nvars x seq_len] # time_index: [bs x timeinfo]
         # norm
@@ -78,15 +76,15 @@ class DGraFormer_backbone(nn.Module):
         indices = indices % 7
 
         adj = self.gc(indices, current_epoch)
-        z = self.gcn1(z, adj)
+        z = self.dcgl1(z, adj)
 
         # do patching
         z = z.unfold(dimension=-1, size=self.patch_len, step=self.stride)  # z: [bs x nvars x patch_num x patch_len]
         z = z.permute(0, 1, 3, 2)  # z: [bs x nvars x patch_len x patch_num]
 
         # model
-        z = self.backbone(z)  # z: [bs x nvars x d_model x patch_num]
-        z = self.head(z)  # z: [bs x nvars x target_window]
+        z = self.mtt(z)  # z: [bs x nvars x d_model x patch_num]
+        z = self.pred(z)  # z: [bs x nvars x target_window]
 
         # denorm
         if self.revin:
@@ -95,15 +93,10 @@ class DGraFormer_backbone(nn.Module):
             z = z.permute(0, 2, 1)
         return z
 
-    def create_pretrain_head(self, head_nf, vars, dropout):
-        return nn.Sequential(nn.Dropout(dropout),
-                             nn.Conv1d(head_nf, vars, 1)
-                             )
 
-
-class DGCN(nn.Module):
+class DCGL(nn.Module):
     def __init__(self, d_gcn, mp_layers, c_in=1, c_out=1, propbeta=0.05):
-        super(DGCN, self).__init__()
+        super(DCGL, self).__init__()
         self.nconv = nconv()
         self.d_gcn = d_gcn
         if d_gcn != 1:
@@ -134,26 +127,31 @@ class DGCN(nn.Module):
         return h
 
 
-class graph_constructor(nn.Module):
+class nconv(nn.Module):
+    def __init__(self):
+        super(nconv, self).__init__()
+
+    def forward(self, x, A):
+        x = torch.einsum('bnsc,bnms->bmsc', (x, A))
+        return x.contiguous()
+
+
+class Graph_constructor(nn.Module):
     def __init__(self, n_vars, d_graph, device, alpha=0.9, num_adj_matrices=7, mask=0.5):
-        super(graph_constructor, self).__init__()
+        super(Graph_constructor, self).__init__()
         self.n_vars = n_vars
 
-        # 读取 CSV 文件，假设文件名为 'cosine_similarity_matrix.csv'
-        file_path = './layers/ETTh1_top3_cosine_similarity_matrix_after_fourier01.csv'
+        file_path = './layers/ETTh1_top3_cosine_similarity_matrix_after_fourier.csv'
         data = pd.read_csv(file_path, index_col=0)
 
-        # 将 DataFrame 转换为 NumPy 数组
         similarity_matrix = data.to_numpy()
         self.init_adj_matrix = torch.tensor(similarity_matrix, device=device, dtype=torch.float32)
 
-        # 预生成节点嵌入矩阵
         self.emb_list1 = nn.ParameterList(
             [nn.Parameter(torch.randn(n_vars, d_graph, device=device)) for _ in range(num_adj_matrices)])
         self.emb_list2 = nn.ParameterList(
             [nn.Parameter(torch.randn(n_vars, d_graph, device=device)) for _ in range(num_adj_matrices)])
 
-        # 用于生成不同的邻接矩阵
         self.lin1 = nn.ModuleList([nn.Linear(d_graph, d_graph) for _ in range(num_adj_matrices)])
         self.lin2 = nn.ModuleList([nn.Linear(d_graph, d_graph) for _ in range(num_adj_matrices)])
         self.lin1.to(device)
@@ -175,19 +173,15 @@ class graph_constructor(nn.Module):
             a = (1 - prop) * self.init_adj_matrix + prop * torch.mm(nodevec1, nodevec2.transpose(1, 0))
             adj = F.relu(torch.tanh(a))
 
-            # 将对角线设置为0
             adj = adj - torch.diag(torch.diag(adj))
 
             values, indices = torch.topk(adj.reshape(-1), int(num_elements * self.mask), largest=True)
 
-            # 创建一个零的mask矩阵
             mask = torch.zeros_like(adj.reshape(-1), device=adj.device)
             mask[indices] = 1  # 选择 topk 的元素，其他位置为 0
 
-            # 使用 mask 来筛选邻接矩阵
             adj = mask.view(adj.size(0), adj.size(1)).view_as(adj) * adj
 
-            # 将对角线重新设置为 1
             adj = adj + torch.eye(adj.size(0)).to(adj.device)
 
             d = adj.sum(1)
@@ -200,33 +194,7 @@ class graph_constructor(nn.Module):
         return dadj
 
 
-class nconv(nn.Module):
-    def __init__(self):
-        super(nconv, self).__init__()
-
-    def forward(self, x, A):
-        x = torch.einsum('bnsc,bnms->bmsc', (x, A))
-        return x.contiguous()
-
-
-class Flatten_Head(nn.Module):
-    def __init__(self, n_vars, nf, target_window, head_dropout=0):
-        super().__init__()
-        self.n_vars = n_vars
-        self.flatten = nn.Flatten(start_dim=-2)
-        self.linear = nn.Linear(nf, target_window)
-        self.dropout = nn.Dropout(head_dropout)
-
-    def forward(self, x):  # x: [bs x nvars x d_model x patch_num]
-
-        x = self.flatten(x)
-        x = self.linear(x)
-        x = self.dropout(x)
-
-        return x
-
-
-class TSTiEncoder(nn.Module):  # i means channel-independent
+class MTT(nn.Module):
     def __init__(self, c_in, patch_num, patch_len, max_seq_len=1024,
                  n_layers=3, d_model=128, n_heads=16, d_k=None, d_v=None,
                  d_ff=256, norm='BatchNorm', attn_dropout=0., dropout=0., act="gelu", store_attn=False,
@@ -239,7 +207,7 @@ class TSTiEncoder(nn.Module):  # i means channel-independent
 
         # Input encoding
         q_len = patch_num
-        self.W_P = nn.Linear(patch_len, d_model)  # Eq 1: projection of feature vectors onto a d-dim vector space
+        self.W_P = nn.Linear(patch_len, d_model)
         self.seq_len = q_len
 
         # Positional encoding
@@ -249,18 +217,21 @@ class TSTiEncoder(nn.Module):  # i means channel-independent
         self.dropout = nn.Dropout(dropout)
 
         # Encoder
-        self.encoder1 = TSTEncoder(q_len, d_model, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                                   attn_dropout=attn_dropout, dropout=dropout,
-                                   pre_norm=pre_norm, activation=act, res_attention=res_attention, n_layers=n_layers,
-                                   store_attn=store_attn)
-        self.encoder2 = TSTEncoder(q_len / 2, d_model * 2, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                                   attn_dropout=attn_dropout, dropout=dropout,
-                                   pre_norm=pre_norm, activation=act, res_attention=res_attention, n_layers=n_layers,
-                                   store_attn=store_attn)
-        self.encoder3 = TSTEncoder(q_len / 4, d_model * 4, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                                   attn_dropout=attn_dropout, dropout=dropout,
-                                   pre_norm=pre_norm, activation=act, res_attention=res_attention, n_layers=n_layers,
-                                   store_attn=store_attn)
+        self.encoder1 = TransformerEncoder(q_len, d_model, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
+                                           attn_dropout=attn_dropout, dropout=dropout,
+                                           pre_norm=pre_norm, activation=act, res_attention=res_attention,
+                                           n_layers=n_layers,
+                                           store_attn=store_attn)
+        self.encoder2 = TransformerEncoder(q_len / 2, d_model * 2, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
+                                           attn_dropout=attn_dropout, dropout=dropout,
+                                           pre_norm=pre_norm, activation=act, res_attention=res_attention,
+                                           n_layers=n_layers,
+                                           store_attn=store_attn)
+        self.encoder3 = TransformerEncoder(q_len / 4, d_model * 4, n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
+                                           attn_dropout=attn_dropout, dropout=dropout,
+                                           pre_norm=pre_norm, activation=act, res_attention=res_attention,
+                                           n_layers=n_layers,
+                                           store_attn=store_attn)
 
     def forward(self, x) -> Tensor:  # x: [bs x nvars x patch_len x patch_num]
 
@@ -274,38 +245,38 @@ class TSTiEncoder(nn.Module):  # i means channel-independent
         u = self.dropout(u + self.W_pos)  # u: [bs * nvars x patch_num x d_model]
 
         # Encoder
-        u = self.encoder1(u)  # z: [bs * nvars x patch_num x d_model]
+        u = self.encoder1(u)  # u: [bs * nvars x patch_num x d_model]
 
         b, n, d = u.shape
         u = u.reshape(b, n // 2, 2 * d)
 
         # Encoder
-        u = self.encoder2(u)  # z: [bs * nvars x patch_num x d_model]
+        u = self.encoder2(u)  # u: [bs * nvars x patch_num / 2 x d_model * 2]
 
         b, n, d = u.shape
         u = u.reshape(b, n // 2, 2 * d)
         # Encoder
-        u = self.encoder3(u)  # z: [bs * nvars x patch_num x d_model]
+        u = self.encoder3(u)  # u: [bs * nvars x patch_num / 4 x d_model * 4]
 
-        u = torch.reshape(u, (-1, n_vars, u.shape[-2], u.shape[-1]))  # z: [bs x nvars x patch_num x d_model]
-        u = u.permute(0, 1, 3, 2)  # z: [bs x nvars x d_model x patch_num]
+        u = torch.reshape(u, (-1, n_vars, u.shape[-2], u.shape[-1]))  # u: [bs * nvars x patch_num / 4 x d_model * 4]
+        u = u.permute(0, 1, 3, 2)  # u: [bs * nvars x d_model * 4 x patch_num / 4]
 
         return u
 
     # Cell
 
 
-class TSTEncoder(nn.Module):
+class TransformerEncoder(nn.Module):
     def __init__(self, q_len, d_model, n_heads, d_k=None, d_v=None, d_ff=None,
                  norm='BatchNorm', attn_dropout=0., dropout=0., activation='gelu',
                  res_attention=False, n_layers=1, pre_norm=False, store_attn=False):
         super().__init__()
 
         self.layers = nn.ModuleList(
-            [TSTEncoderLayer(q_len, d_model, n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                             attn_dropout=attn_dropout, dropout=dropout,
-                             activation=activation, res_attention=res_attention,
-                             pre_norm=pre_norm, store_attn=store_attn) for i in range(n_layers)])
+            [TransformerEncoderLayer(q_len, d_model, n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
+                                     attn_dropout=attn_dropout, dropout=dropout,
+                                     activation=activation, res_attention=res_attention,
+                                     pre_norm=pre_norm, store_attn=store_attn) for i in range(n_layers)])
         self.res_attention = res_attention
 
     def forward(self, src: Tensor, key_padding_mask: Optional[Tensor] = None, attn_mask: Optional[Tensor] = None):
@@ -320,7 +291,7 @@ class TSTEncoder(nn.Module):
             return output
 
 
-class TSTEncoderLayer(nn.Module):
+class TransformerEncoderLayer(nn.Module):
     def __init__(self, q_len, d_model, n_heads, d_k=None, d_v=None, d_ff=256, store_attn=False,
                  norm='BatchNorm', attn_dropout=0, dropout=0., bias=True, activation="gelu", res_attention=False,
                  pre_norm=False):
@@ -509,3 +480,20 @@ class _ScaledDotProductAttention(nn.Module):
             return output, attn_weights, attn_scores
         else:
             return output, attn_weights
+
+
+class Predictor(nn.Module):
+    def __init__(self, n_vars, nf, target_window, head_dropout=0):
+        super().__init__()
+        self.n_vars = n_vars
+        self.flatten = nn.Flatten(start_dim=-2)
+        self.linear = nn.Linear(nf, target_window)
+        self.dropout = nn.Dropout(head_dropout)
+
+    def forward(self, x):  # x: [bs x nvars x d_model x patch_num]
+
+        x = self.flatten(x)
+        x = self.linear(x)
+        x = self.dropout(x)
+
+        return x
