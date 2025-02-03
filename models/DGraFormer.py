7a4ecf647b1be307d4878f5156097d979ab3e913
Yan  Han <143077869+yh-Hanniel@users.noreply.github.com>
2025-02-03 23:45:46 +0800

    Add files via upload

--- models/DGraFormer.py
+++ models/DGraFormer.py
@@ -3,10 +3,10 @@ __all__ = ['DGraFormer']
 # Cell
 from typing import Optional
 
-from layers.DGraFormer_backbone import DGraFormer_backbone
-from layers.DGraFormer_layers import series_decomp
 from torch import Tensor
 from torch import nn
+
+from layers.DGraFormer_backbone import DGraFormer_backbone
 
 
 class Model(nn.Module):
@@ -16,7 +16,6 @@ class Model(nn.Module):
                  attn_mask: Optional[Tensor] = None, res_attention: bool = True,
                  pre_norm: bool = False, store_attn: bool = False, pe: str = 'zeros', learn_pe: bool = True,
                  pretrain_head: bool = False, head_type='flatten', verbose: bool = False, **kwargs):
-
         super().__init__()
 
         # load parameters
@@ -32,8 +31,6 @@ class Model(nn.Module):
         fc_dropout = configs.fc_dropout
         head_dropout = configs.head_dropout
 
-        individual = configs.individual
-
         patch_len = configs.patch_len
         stride = configs.stride
         padding_patch = configs.padding_patch
@@ -41,71 +38,33 @@ class Model(nn.Module):
         revin = configs.revin
         affine = configs.affine
         subtract_last = configs.subtract_last
-
-        decomposition = configs.decomposition
-        kernel_size = configs.kernel_size
-
         device = configs.device
+        d_graph = configs.d_graph
+        d_gcn = configs.d_gcn
+        w_ratio = configs.w_ratio
+        mp_layers = configs.mp_layers
 
         # model
-        self.decomposition = decomposition
-        if self.decomposition:
-            self.decomp_module = series_decomp(kernel_size)
-            self.model_trend = DGraFormer_backbone(c_in=c_in, context_window=context_window, target_window=target_window,
-                                                 patch_len=patch_len, stride=stride,
-                                                 mdevice=device,ax_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                                 n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                                                 attn_dropout=attn_dropout,
-                                                 dropout=dropout, act=act, key_padding_mask=key_padding_mask,
-                                                 padding_var=padding_var,
-                                                 attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm,
-                                                 store_attn=store_attn,
-                                                 pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout,
-                                                 head_dropout=head_dropout, padding_patch=padding_patch,
-                                                 pretrain_head=pretrain_head, head_type=head_type,
-                                                 individual=individual, revin=revin, affine=affine,
-                                                 subtract_last=subtract_last, verbose=verbose, **kwargs)
-            self.model_res = DGraFormer_backbone(c_in=c_in, context_window=context_window, target_window=target_window,
-                                               patch_len=patch_len, stride=stride,
-                                               device=device,max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                               n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                                               attn_dropout=attn_dropout,
-                                               dropout=dropout, act=act, key_padding_mask=key_padding_mask,
-                                               padding_var=padding_var,
-                                               attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm,
-                                               store_attn=store_attn,
-                                               pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout,
-                                               head_dropout=head_dropout, padding_patch=padding_patch,
-                                               pretrain_head=pretrain_head, head_type=head_type, individual=individual,
-                                               revin=revin, affine=affine,
-                                               subtract_last=subtract_last, verbose=verbose, **kwargs)
-        else:
-            self.model = DGraFormer_backbone(c_in=c_in, context_window=context_window, target_window=target_window,
-                                           patch_len=patch_len, stride=stride,
-                                           device=device,max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                           n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
-                                           attn_dropout=attn_dropout,
-                                           dropout=dropout, act=act, key_padding_mask=key_padding_mask,
-                                           padding_var=padding_var,
-                                           attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm,
-                                           store_attn=store_attn,
-                                           pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout,
-                                           padding_patch=padding_patch,
-                                           pretrain_head=pretrain_head, head_type=head_type, individual=individual,
-                                           revin=revin, affine=affine,
-                                           subtract_last=subtract_last, verbose=verbose, **kwargs)
+
+        self.model = DGraFormer_backbone(c_in=c_in, context_window=context_window, target_window=target_window,
+                                         patch_len=patch_len, stride=stride,
+                                         device=device, max_seq_len=max_seq_len, d_graph=d_graph, d_gcn=d_gcn,
+                                         w_ratio=w_ratio, mp_layers=mp_layers, n_layers=n_layers, d_model=d_model,
+                                         n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm,
+                                         attn_dropout=attn_dropout,
+                                         dropout=dropout, act=act, key_padding_mask=key_padding_mask,
+                                         padding_var=padding_var,
+                                         attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm,
+                                         store_attn=store_attn,
+                                         pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout,
+                                         padding_patch=padding_patch,
+                                         pretrain_head=pretrain_head, head_type=head_type,
+                                         revin=revin, affine=affine,
+                                         subtract_last=subtract_last, verbose=verbose, **kwargs)
 
     def forward(self, x, time_index, current_epoch):  # x: [Batch, Input length, Channel]
-        if self.decomposition:
-            res_init, trend_init = self.decomp_module(x)
-            res_init, trend_init = res_init.permute(0, 2, 1), trend_init.permute(0, 2,
-                                                                                 1)  # x: [Batch, Channel, Input length]
-            res = self.model_res(res_init)
-            trend = self.model_trend(trend_init)
-            x = res + trend
-            x = x.permute(0, 2, 1)  # x: [Batch, Input length, Channel]
-        else:
-            x = x.permute(0, 2, 1)  # x: [Batch, Channel, Input length]
-            x = self.model(x, time_index, current_epoch)
-            x = x.permute(0, 2, 1)  # x: [Batch, Input length, Channel]
+
+        x = x.permute(0, 2, 1)  # x: [Batch, Channel, Input length]
+        x = self.model(x, time_index, current_epoch)
+        x = x.permute(0, 2, 1)  # x: [Batch, Input length, Channel]
         return x
